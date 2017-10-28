90f8b9b59ab64def9c2348c4c8f72b09216dbcda
luuhoang <luugu196@gmail.com>
2017-10-28 16:50:40 +0700

    Customer

--- index.js
+++ index.js
@@ -26,8 +26,29 @@ const getDrivers = () => new Promise((resolve, reject) => {
 	});
 });
 
-const toCSV = (drivers) => {
-	return arrayToCSV(drivers);
+const getCustomers = () => new Promise((resolve, reject) => {
+	mysqlConnection.query('SELECT customer.* FROM customer', (err, rows) => {
+		if (err) return reject(err);
+		if (!rows[0]) {
+			return resolve([]);
+		}
+		let result = [];
+		result.push(['ID', 'Name', 'Last name', 'Phone', 'Email']);
+		rows.forEach(row => {
+			result.push([
+				row.customerId,
+				row.name,
+				row.lastName,
+				row.phone,
+				row.email
+			]);
+		});
+		resolve(result);
+	});
+});
+
+const toCSV = (list) => {
+	return arrayToCSV(list);
 };
 
 const main = async () => {
@@ -35,6 +56,9 @@ const main = async () => {
 		let drivers = await getDrivers();
 		let csv = toCSV(drivers);
 		fs.writeFileSync('drivers.csv', csv, 'utf8');
+		let customers = await getCustomers();
+		csv = toCSV(customers);
+		fs.writeFileSync('customers.csv', csv, 'utf8');
 		console.log("Done");
 	} catch (err) {
 		console.error(err);
